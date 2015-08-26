3818d1a58be515b4808c361e5a3e8ddd6dfe0a57
Brian Johnson <johnba4@gmail.com>
2015-08-26 10:57:54 -0400

    ran bundle install and updated route file to send root to users#new

--- mailerApp/config/routes.rb
+++ mailerApp/config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root 'welcome#index'
+  root 'users#new'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
